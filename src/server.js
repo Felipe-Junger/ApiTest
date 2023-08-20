63dae85cf77c0fa426462b0c5e309a967f0a6ed8
Felipe <felipejunger7@gmail.com>
2023-08-19 23:18:52 -0300

    testanto o Route Params

--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', ( request, response ) => {
-   response.send('Hello World!');
+app.get('/message/:id', ( request, response ) => {
+   
+   response.send(`Id da mensagem: ${request.params.id}`);
 });
 
 const PORT = 3333;
