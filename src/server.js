868d15c52645797f4bea42f6cd07365b476dafec
Felipe <felipejunger7@gmail.com>
2023-08-20 22:35:03 -0300

    criando arquivo para rotas e variável userRoutes

--- src/server.js
+++ src/server.js
@@ -4,12 +4,6 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
-app.post('/users', ( request, response ) => {
-   const { name, email, password } = request.body;
-   
-   response.json({ name, email, password });
-});
-
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
