d8734b32792edd7d8411ebc0685ac09ae25f0c15
Felipe <felipejunger7@gmail.com>
2023-08-20 22:55:16 -0300

    importando const routes no server da aplicação

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+const routes = require('./routes');
+
 const app = express();
 app.use(express.json());
+
+app.use(routes);
 
 const PORT = 3333;
 
