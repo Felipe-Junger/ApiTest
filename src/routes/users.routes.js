f893dad8c84a2a800513d187272a8af4194530cf
Felipe <felipejunger7@gmail.com>
2023-08-22 23:09:22 -0300

    criando class AppError

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,18 +4,8 @@ const UsersController = require('../controllers/UsersController');
 
 const userRoutes = Router();
 
-function myMiddleware ( request, response, next ) {
-    console.log('Você passou pelo middleware!');
-    if (!request.body.isAdmin) {
-        return response.json ({ message: "User not authorized" });
-    }
-
-    next();
-}
-
-
 const usersController = new UsersController();
 
-userRoutes.post('/', myMiddleware, usersController.create);
+userRoutes.post('/', usersController.create);
 
 module.exports = userRoutes;
---
+++ src/utils/AppError.js
@@ -0,0 +1,7 @@
+class AppError {
+    mesage;
+    statusCode;
+
+    
+    constructor(message, statusCode = 400);
+}
