413dd7f9058671ebd4d33837ef09114b00594579
Felipe <felipejunger7@gmail.com>
2023-08-22 23:44:13 -0300

    criando app.use para AppError

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,6 +3,10 @@ const AppError = require('../utils/AppError');
 class UsersController {
     async create ( request, response) {
         const { name, email, password } = request.body;
+
+        if (!name) {
+            throw new AppError('O nome é obrigatório!')
+        }
     
         response.status(201).json({ name, email, password });
     }
