f1de3bac8b699feaf020505c0b5185f4a6816b71
Felipe <felipejunger7@gmail.com>
2023-08-23 23:22:30 -0300

    importando e ativando database no server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('express-async-errors');
-
+const database = require('./database/sqlite');
 const AppError = require('./utils/AppError');
 
 const express = require('express');
@@ -12,6 +12,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use(( error, request, response, next ) => {
     if(error instanceof AppError) {
